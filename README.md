30ab0d5a85a5501b76a737dc0e1444856aa0bce9
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-05-04 23:03:56 -0400

    Added new floors

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Link to website:
 https://cisc3650groupproject.github.io/website/
 
-Link to Directory:
-https://cisc3650groupproject.github.io/website/directory/laguardia_hall_library.html
+Link to the directory:
+https://cisc3650groupproject.github.io/website/directory
 
 ## Credit
 [Brooklyn College map](https://www.brooklyn.cuny.edu/web/about/history/masterplan.php)
--- js/laguardia_hall_library.js
+++ js/laguardia_hall_library.js
@@ -27,12 +27,20 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) 
+  {
+    slideIndex = 1;
+  }
+  if (n < 1) 
+  {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) 
+  {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) 
+  {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
@@ -51,7 +59,6 @@ function closeModal2() {
   document.getElementById("myModal2").style.display = "none";
 }
 
-var slideIndex = 1;
 showSlides2(slideIndex);
 
 // Next/previous controls
@@ -94,7 +101,6 @@ function closeModal3() {
   document.getElementById("myModal3").style.display = "none";
 }
 
-var slideIndex = 1;
 showSlides3(slideIndex);
 
 // Next/previous controls
@@ -118,6 +124,104 @@ function showSlides3(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+//Third Floor
+// Open the Modal
+function openModal4() {
+  document.getElementById("myModal4").style.display = "block";
+}
+
+// Close the Modal
+function closeModal4() {
+  document.getElementById("myModal4").style.display = "none";
+}
+
+showSlides4(slideIndex);
+
+// Next/previous controls
+function plusSlides4(n) {
+  showSlides4(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide4(n) {
+  showSlides4(slideIndex = n);
+}
+
+function showSlides4(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides4");
+  var dots = document.getElementsByClassName("demo4");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) 
+  {
+    slideIndex = 1
+  }
+  if (n < 1) 
+  {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) 
+  {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+//Fourth Floor
+// Open the Modal
+function openModal5() {
+  document.getElementById("myModal5").style.display = "block";
+}
+
+// Close the Modal
+function closeModal5() {
+  document.getElementById("myModal5").style.display = "none";
+}
+
+showSlides5(slideIndex);
+
+// Next/previous controls
+function plusSlides5(n) {
+  showSlides5(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide5(n) {
+  showSlides5(slideIndex = n);
+}
+
+function showSlides5(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides5");
+  var dots = document.getElementsByClassName("demo5");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) 
+  {
+    slideIndex = 1
+  }
+  if (n < 1) 
+  {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) 
+  {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
